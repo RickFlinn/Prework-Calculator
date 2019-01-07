9a4a176f97b384e97d0f8997458234b161c50c2b
BehelitFam <mriodine@gmail.com>
2019-01-07 11:25:04 -0800

    refactored what I send to console

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -9,45 +9,47 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Dis a Calculalator. Good fer all yer basic Calculalatin' needs!");
-            Console.WriteLine($"4 + 3 = {Summify(4, 3)}");
-            Console.WriteLine($"7 * 8 = {Multiplerate(7, 8)}");
-            Console.WriteLine($"20 / 3 = {Dividerize(20, 3)}");
-            Console.WriteLine($"87 - 27.5 = {Subtractidate(87, 27.5)}");
-            Console.WriteLine($"8 squared is = {SquareButLikeNotTheDance(8)}");
+            Summify(4, 3);
+            Multiplerate(7, 8);
+            Dividerize(20, 3);
+            Subtractidate(9000, -1);
+            SquareButLikeNotTheDance(8);
+            Factorialify(9);
+            CosignNotYourLease(.5);
         }
 
         // Adds two numbers and returns the sum.
         public static double Summify (double x, double y)
         {
-            Console.WriteLine("" + (x + y));
+            Console.WriteLine($"{x} + {y} = {(x + y)}");
             return x + y;
         }
 
         // Multiplies two numbers and returns the product.
         public static double Multiplerate (double x, double y)
         {
-            Console.WriteLine("" + (x * y));
+            Console.WriteLine($"{x} * {y} = {(x * y)}");
             return x * y;
         }
 
         // Divides two numbers and returns whatever you call something you just divided
         public static double Dividerize (double x, double y)
         {
-            Console.WriteLine("" + (x / y));
+            Console.WriteLine($"{x} / {y} = {(x / y)}");
             return x / y;
         }
         
         // Divides two numbers and yeah my math terminology is weak, where is Hannah when you need her
         public static double Subtractidate (double x, double y)
         {
-            Console.WriteLine("" + (x - y));
+            Console.WriteLine($"{x} - {y} = {(x - y)}");
             return x - y;
         }
 
         // Squares given number and returns the square. No dancing involved here, take your damned countryside barn revelry elsewhere
         public static double SquareButLikeNotTheDance(double x)
         {
-            Console.WriteLine("" + (x * x));
+            Console.WriteLine($"{x} squared is {(x * x)}");
             return x * x;
         }
 
@@ -55,19 +57,19 @@ namespace Calculator
         public static double TheRootOfTheSquare (double x)
         {
             double ans = Math.Sqrt(x);
-            Console.WriteLine(ans);
+            Console.WriteLine($"The square root of {x} is {ans}");
             return ans;
         }
 
         // Factorializes a given integer. 
-        public static int Factoryilize(int x)
+        public static int Factorialify(int x)
         {
             int ans = 1;
             for (int i = x; i > 0; i--)
             {
                 ans *= i;
             }
-            Console.WriteLine("" + ans);
+            Console.WriteLine($"{x}! is {ans}");
             return ans;
         }
 
@@ -75,7 +77,7 @@ namespace Calculator
         public static double CosignNotYourLease (double x)
         {
             double ans = Math.Cos(x);
-            Console.WriteLine(ans);
+            Console.WriteLine($"The cosine of {x} is {ans}");
             return ans;
         }
     }
